fd672ee81def65b8a5f08a6e5335400ddf32df48
AnastasyaOsipova <nastasjaosipova@yandex.ru>
2022-10-15 21:42:14 +0300

    fixed likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -53,7 +53,7 @@ function App(props) {
   const [cards, setCards] = React.useState([]);
 
   function handleCardLike(likes, _id) {
-    const isLiked = likes.some((i) => i.owner === currentUser._id);
+    const isLiked = likes.some((i) => i === currentUser._id);
 
     api
       .changeLikeCardStatus(_id, isLiked)
